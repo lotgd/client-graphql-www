f2afbd214033737b78e64a725cea9f9be3acf109
Vassyli <basilius.sauter@gmail.com>
2017-03-13 21:46:49 +0100

    Added character creation including update procedure and error handling

---
+++ app/components/CharacterCreation.js
@@ -0,0 +1,49 @@
+import React, {Component, PropTypes} from 'react';
+import {graphql} from 'react-apollo';
+
+import Message from './parts/Message';
+import Form from './../../src/Form';
+
+
+class CharacterCreation extends Component {
+    static PropType = {
+        userId: PropTypes.string.isRequired,
+        displayForm: PropTypes.bool.isRequired,
+        onCreate: PropTypes.func.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const form = [
+            {type: "text", label: "character name", fieldName: "name", defaultValue: ""}
+        ];
+
+        return <div>
+            <CreationButton toggle={this.props.toggled} onClick={this.props.onToggle} />
+
+            {this.props.creationMessage && this.props.toggled && <Message
+                message={this.props.creationMessage}
+                error={this.props.creationError}
+            />}
+
+            {this.props.toggled && <Form
+                fields={form}
+                submitButton="Create"
+                onSubmit={this.props.onCreate}
+            />}
+        </div>;
+    }
+}
+
+function CreationButton(props) {
+    const content = props.toggle ? "^" : "+";
+
+    return (
+        <div className="d-roundButton" onClick={() => {props.onClick();}}>{content}</div>
+    );
+}
+
+export default CharacterCreation;
--- app/components/CharacterSelect.js
+++ app/components/CharacterSelect.js
@@ -1,9 +1,12 @@
 import React, {Component, PropTypes} from 'react';
 import {graphql} from 'react-apollo';
+import update from 'immutability-helper';
 
+import CharacterCreation from './CharacterCreation';
 import Message from './parts/Message';
 
-import CharacterQuery from './../queries/CharacterQuery.graphql';
+import getCharactersForUser from '../queries/getCharactersForUser.graphql';
+import createCharacterMutation from './../mutations/createCharacterMutation.graphql';
 
 class CharacterEntry extends Component {
     static PropType = {
@@ -25,12 +28,31 @@ class CharacterEntry extends Component {
     }
 }
 
-@graphql(CharacterQuery, {
+@graphql(getCharactersForUser, {
     options: (ownProps) => ({
         variables: {
             userId: ownProps.userId
         }
     })
+})
+@graphql(createCharacterMutation, {
+    name: "createCharacterMutation",
+    options: {
+        updateQueries: {
+            getCharactersForUser: (prev, {mutationResult}) => {
+                const result = mutationResult.data.createCharacter;
+                if (prev.user.id === result.user.id) {
+                    return update(prev, {
+                        user: {
+                            characters: {
+                                $push: [result.character]
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
 })
 class CharacterSelect extends Component {
     static PropType = {
@@ -45,10 +67,68 @@ class CharacterSelect extends Component {
     };
 
     static defaultProps = {
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            creationMessage: null,
+            creationError: null,
+            creationSuccess: null,
+        }
     }
 
     onActivation(characterId) {
         this.props.characterSelect(characterId);
+    }
+
+    onCreate(data) {
+        this.setState({
+            creationMessage: "Character is being created...",
+            creationError: null,
+            creationSuccess: null,
+        });
+
+        let a = this.props.createCharacterMutation({
+            variables: {
+                input: {
+                    userId: this.props.userId,
+                    characterName: data["name"],
+                }
+            }
+        }).then(this.onCreateSuccess.bind(this)).catch(this.onCreateFailure.bind(this));
+    }
+
+    onCreateSuccess(data) {
+        this.setState({
+            creationMessage: "Character has been successfully created.",
+            creationSuccess: true,
+        });
+
+        this.toggleForm(false);
+    }
+
+    onCreateFailure(error) {
+        let message = "Character creation was not successful";
+        if (error.graphQLErrors && error.graphQLErrors.length > 0) {
+            message += ": " + error.graphQLErrors[0].message + ".";
+        } else {
+            message += ".";
+        }
+
+        this.setState({
+            creationMessage: message,
+            creationError: true,
+        });
+    }
+
+    toggleForm(to = null) {
+        if (to) {
+            this.setState({toggled: to});
+        } else {
+            this.setState({toggled: !this.state.toggled});
+        }
     }
 
     render() {
@@ -63,21 +143,34 @@ class CharacterSelect extends Component {
 
         const characters = this.props.data.user.characters;
 
-        return <div className="d-cardList">
-            {characters.length > 0 && characters.map((character) => {
-                let selected = false;
+        return <div>
+            <div className="d-cardList">
+                {characters.length > 0 && characters.map((character) => {
+                    let selected = false;
 
-                if (this.props.selectedCharacter === character.id) {
-                    selected = true;
-                }
+                    if (this.props.selectedCharacter === character.id) {
+                        selected = true;
+                    }
 
-                return <CharacterEntry
-                    key={character.id}
-                    character={character}
-                    onActivation={this.onActivation.bind(this, character.id)}
-                    selected={selected}
-                />;
-            })}
+                    return <CharacterEntry
+                        key={character.id}
+                        character={character}
+                        onActivation={this.onActivation.bind(this, character.id)}
+                        selected={selected}
+                    />;
+                })}
+                {characters.length === 0 && <Message message="You don't appear to have any characters. Want to create one?" />}
+            </div>
+
+            <CharacterCreation
+                userId={this.props.userId}
+                displayForm={characters.length === 0}
+                toggled={this.state.toggled}
+                creationMessage={this.state.creationMessage}
+                creationError={this.state.creationError}
+                onCreate={this.onCreate.bind(this)}
+                onToggle={this.toggleForm.bind(this)}
+            />
         </div>
     }
 }
